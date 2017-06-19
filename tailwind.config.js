e91d88c41ec88041cdd88c0fd56235477ae49092
cindy0518 <scichen@ucdavis.edu>
2017-06-19 22:46:03 +0800

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'extend-red': '#FF1A42',
+        'extend-teal': '#1EF7C0',
+        'extend-yellow': '#F7E32A',
+        'extend-blue': '#34C2FF',
+      },
+    },
+  },
+  plugins: [],
+};
